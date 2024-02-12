0376e81649d028e11dda94efbfe2a37a681dbe83
nikesh <nikesh.tadela@gmail.com>
2024-02-12 23:30:51 +0530

    working prototype

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -5,8 +5,11 @@ import Input from './Input.jsx';
 export default function Entry (props){
     return (
         <div className='entry'>
-            <Input className='startDate' callback={props.callback} id={props.id} value={props.startDate}/>
-            <Input className='endDate' callback={props.callback}id={props.id} value={props.endDate}/>
+            <div className='duration'>
+                <Input className='startDate' callback={props.callback} id={props.id} value={props.startDate}/>
+                <p>-</p>
+                <Input className='endDate' callback={props.callback}id={props.id} value={props.endDate}/>
+            </div>
             <Input className='title' callback={props.callback}id={props.id} value={props.title}/>
             <Input className='desc' callback={props.callback}id={props.id} value={props.desc}/>
         </div>
